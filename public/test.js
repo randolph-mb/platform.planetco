1e7dd9c7a5bd5239d898941824bedb30a4c331ec
m1000fs <145189741+m1000fs@users.noreply.github.com>
2025-11-23 19:02:00 +0100

    Cleanup: Remove test files

--- public/test.js
+++
@@ -1,5 +0,0 @@
-console.log('Test JS running');
-document.body.style.backgroundColor = 'green';
-const div = document.createElement('div');
-div.innerText = 'JS WORKED';
-document.body.appendChild(div);
