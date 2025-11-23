4f065f0b720cd1bac482bb33b3d8f7fa24c688bc
m1000fs <145189741+m1000fs@users.noreply.github.com>
2025-11-23 19:00:15 +0100

    Fix: Set base path to relative for GitHub Pages

---
+++ public/test.js
@@ -0,0 +1,5 @@
+console.log('Test JS running');
+document.body.style.backgroundColor = 'green';
+const div = document.createElement('div');
+div.innerText = 'JS WORKED';
+document.body.appendChild(div);
